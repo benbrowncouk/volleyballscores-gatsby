09169c7c45352e766a3d27950a0d37de2cf2d4e6
Ben Brown <ben@ben-brown.co.uk>
2018-11-12 16:54:45 +0000

    Update config for PWA settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,11 +9,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'VolleyballScores.co.uk',
+        short_name: 'Volleyball Scores',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#4a87d3',
+        theme_color: '#4a87d3',
         display: 'minimal-ui',
         icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
       },
