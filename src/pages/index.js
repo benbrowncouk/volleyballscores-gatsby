765a7dfde6b7c928027aa3a34e15424eab3f4133
Ben Brown <ben@ben-brown.co.uk>
2018-11-19 10:10:31 +0000

    Second attempt to fix Netlify captcha on form

--- src/pages/index.js
+++ src/pages/index.js
@@ -138,7 +138,7 @@ class Homepage extends React.Component {
                               <label htmlFor="message">Message</label>
                               <textarea name="message" id="message" rows="6"></textarea>
                           </div>
-                          <div data-netlify-recaptcha></div>
+                          <div data-netlify-recaptcha="true"></div>
                           <ul className="actions">
                               <li><input type="submit" value="Send Message" className="special" /></li>
                               <li><input type="reset" value="Clear" /></li>
